bff832935f626b0aa82c131fd08d889a453beca2
Caio César Silva <caiocesarsilva08@gmail.com>
2019-10-30 18:10:14 -0300

    Prova...

--- PROVAS/PROVAS 2/2017-2-TW/prova.c
+++ PROVAS/PROVAS 2/2017-2-TW/prova.c
@@ -37,10 +37,6 @@ float randFloat(int min, int max){
     return min + (max-min)*((float)rand()/RAND_MAX);
 }
 
-void initProduto(int min, int max){
-    return min + (max-min) + ((float)rand()/RAND_MAX);
-}
-
 void initProduto(Produto *p, int cod){
     p->id = cod;
     p->estoque = rand()%MAXESTOQUE;
@@ -56,8 +52,43 @@ int estoqueLoja(Produto p[], int n){
     return cont;
 }
 
+int simulaCompra(Produto p[], int n){
+    float total = 0;
+    //variável para definir um produto específico
+    int prod;
+    //numero de produtos que o cliente irá comprar
+    int numCompra = rand()%MAXCOMPRAS;
+    //estoque total
+    int estoqueTotal = estoqueLoja(p, n);
+    while(numCompra > 0 && estoqueTotal > 0){
+        //produto aleatório dentro dos n produtos diferentes que a loja vende
+        int prod = rand()%n;
+        if(p[prod].estoque > 0){
+            p[prod].estoque--;
+            numCompra--;
+            estoqueTotal--;
+            total = total + p[prod].preco;
+        } //else {
+           // printf("\nestoque produto %d acabou!", prod);
+        //}
+    }
+    //if(numCompra == 0){
+    //    printf("\nNão temos mais produtos");
+    //}
+    return total;
+}
+
 int main() {
-    
+    int i = 0, numProdutos = 0, numClientes = 0;
+    Produto p[1000];
+    for(i = 0; i < 1000; i++){
+        initProduto(&p[i], i);
+        //printf("produto %d -> / estoque: %d / preco: %f\n", p[i].id, p[i].estoque, p[i].preco);
+    }
+    numProdutos = 1+rand()%MAXPRODUTOS;
+    numClientes = rand()%MAXCLIENTES;
+
+    printf("Faturamento do dia: %d\n", simulaCompra(p, 1000));
 
     return 0;
 }
