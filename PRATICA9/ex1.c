fbf1d3b516bf6476d593423a16d1e88519c91e60
Caio César <caiocesarsilva08@gmail.com>
2019-10-29 22:56:10 -0300

    PRATICA 9
    Pratica 9 de Structs

--- PRATICA9/ex1.c
+++ PRATICA9/ex1.c
@@ -23,11 +23,12 @@ int rolaDados(){
 	return somaDados;
 }
 
-void criaGuerreiro(guerreiro *g1){
+void criaGuerreiro(guerreiro *g1, int id){
 	g1->ataque = rolaDado();
 	g1->defesa = rolaDado();
 	g1->carisma = rolaDado();
 	g1->pontos_vida = rolaDados();
+	g1->id_jogador = id;
 }
 
 void bonusCarisma(guerreiro *g1){
@@ -44,20 +45,43 @@ void bonusCarisma(guerreiro *g1){
 	} else if(g1->carisma == 18){
 		g1->carisma = g1->carisma + 3;
 	}
-	return g1->carisma;
+}
+
+void ataca(guerreiro *g1, guerreiro *g2){
+	int golpe, escudo, dano;
+	golpe = g1->ataque + rolaDados() + g1->carisma;
+	escudo = rolaDados() + g2->defesa + g2->carisma;
+	dano = golpe - escudo;
+	if (dano > 0){
+		g2->pontos_vida -= dano;
+	}
 }
 
 int main(){
-	
+	int i;
 	guerreiro x;
-	criaGuerreiro(&x);
-	printf("carisma antigo: %i\n", x.carisma);
-	bonusCarisma(&x);
+	guerreiro y;
+
+	criaGuerreiro(&x, 1);
+	criaGuerreiro(&y, 2);
 	
-	printf("ataque: %i\n", x.ataque);
-	printf("defesa: %i\n", x.defesa);
-	printf("carisma:  %i\n", x.carisma);
-	printf("pontos de vida: %i\n", x.pontos_vida);
+	bonusCarisma(&x);
+	bonusCarisma(&y);
+	
+	for(i = 0; x.pontos_vida > 0 && y.pontos_vida > 0; i++){
+		if(i % 2 == 0){
+			ataca(&x, &y);
+		} else {
+			ataca(&y, &x);
+		}
+	}
+
+	if(x.pontos_vida <= 0){
+		printf("Quem ganhou foi o guerreiro: %d\n", x.id_jogador);
+	}
+	if(y.pontos_vida <= 0){
+		printf("Quem ganhou foi o guerreiro: %d\n", y.id_jogador);
+	}
 
 	return 0;
 }
